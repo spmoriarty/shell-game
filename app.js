ce86b2aa0b4ec1a1d9a7702cedeb79f7e5174230
Sean Moriarty <spmoriarty1@gmail.com>
2022-04-26 16:33:37 -0700

    retooled with Katie for buttons and events

--- app.js
+++ app.js
@@ -4,50 +4,57 @@
 
 
 // let state
-
+//Button
 const button = document.getElementById('Choose1');
 const button2 = document.getElementById('Choose2');
 const button3 = document.getElementById('Choose3');
 
+
+// Results
 const Wisely = document.getElementById('Wisely');
 const Poorly  = document.getElementById('Poorly');
 const Total = document.getElementById('Total');
 
 let count = 0;
-
+//Remove -reset
 button.addEventListener('click', () => {
-  Choose1.classlist.remove('Choice'),
-  Choose2.classlist.remove('Choice'),
-  Choose3.classlist.remove('Choice'),
+  button.classlist.remove('grenade');
+  button2.classlist.remove('grenade');
+  button3.classlist.remove('grenade');
+});
 
-
-const randomized = Math.ceil(Math.random() * 3);
-if (randomized === 1){
-Choose1.classList.add('choice');
-Wisely++;
-}
-else if (randomized === 2) {
-Choose2.clasList.add('choice');
-Poorly++;
-}
-else if (randomized === 3); {
-Choose3.classList.add('choice')
-Poorly++;
-}
 
 button.addEventListener('click', () => {
     console.log('You have chosen');
+    randomized ();
     count++;
     Total.textContent = count;
 });
 button2.addEventListener('click', () => {
     console.log('You have chosen');
+    randomized ();
     count++;
     Total.textContent = count;
 });
   
 button3.addEventListener('click', () => {
     console.log('You have chosen');
+    randomized ();
     count++;
     Total.textContent = count;
 });
+
+
+let randomized = Math.ceil(Math.random() * 3); {
+if (randomized === 1){
+Choose1.classList.add('grenade');
+Wisely++;
+}
+else if (randomized === 2) {
+Choose2.classList.add('grenade');
+Poorly++;
+}
+else if (randomized === 3); {
+Choose3.classList.add('grenade')
+Poorly++;
+}};
