f4acded0d0c09f9f03b657d3e2d8e6df43db5160
Sean Moriarty <spmoriarty1@gmail.com>
2022-04-26 11:34:26 -0700

    Added randomizer and choice class

--- app.js
+++ app.js
@@ -5,15 +5,35 @@
 
 // let state
 
-const button = document.getElementById('Choose1', );
-const button2 = document.getElementById('Choose2', );
-const button3 = document.getElementById('Choose3', );
+const button = document.getElementById('Choose1');
+const button2 = document.getElementById('Choose2');
+const button3 = document.getElementById('Choose3');
 
 const Wisely = document.getElementById('Wisely');
 const Poorly  = document.getElementById('Poorly');
-const Total = document.getElementById('Total')
+const Total = document.getElementById('Total');
 
 let count = 0;
+
+button.addEventListener('click', () => {
+  Choose1.classlist.remove('Choice'),
+  Choose2.classlist.remove('Choice'),
+  Choose3.classlist.remove('Choice'),
+
+
+const randomized = Math.ceil(Math.random() * 3);
+if (randomized === 1){
+Choose1.classList.add('choice');
+Wisely++;
+}
+else if (randomized === 2) {
+Choose2.clasList.add('choice');
+Poorly++;
+}
+else if (randomized === 3); {
+Choose3.classList.add('choice')
+Poorly++;
+}
 
 button.addEventListener('click', () => {
     console.log('You have chosen');
@@ -21,20 +41,13 @@ button.addEventListener('click', () => {
     Total.textContent = count;
 });
 button2.addEventListener('click', () => {
-  console.log('You have chosen');
-  count++;
-  Total.textContent = count;
-  });
-  
-  button3.addEventListener('click', () => {
     console.log('You have chosen');
     count++;
     Total.textContent = count;
-    });
-
-
-
-// set event listeners 
-  // get user input
-  // use user input to update state 
-  // update DOM to reflect the new state
+});
+  
+button3.addEventListener('click', () => {
+    console.log('You have chosen');
+    count++;
+    Total.textContent = count;
+});
